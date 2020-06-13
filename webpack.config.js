8002fa4fa8ddd0d742f6c7fe55194c9db5aeab00
Gilvan Henrique <gilvanh.15@gmail.com>
2020-06-13 15:19:12 -0300

    babel and webpack configurations

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+// o path é usado para padronizar os caminhos independente do SO
+const path = require('path')
+
+// o output define para onde os códigos transpilados serão salvos (public)
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+};
